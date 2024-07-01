6522b811b079baabbdf07999f5ed15981e5a3ddb
mithreshvar <mithreshvar@gmail.com>
2024-07-01 17:49:29 +0530

    edit interaction added

--- src/components/Role/RoleDetail.jsx
+++ src/components/Role/RoleDetail.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { MdModeEditOutline } from 'react-icons/md';
-import { useLoaderData, useParams } from 'react-router-dom';
+import { useLoaderData, useNavigate, useParams } from 'react-router-dom';
 
 const RoleDetail = () => {
     const { id } = useParams();
+    const navigate = useNavigate();
     const role = useLoaderData();
     if (!role) {
         return <div>Role not found</div>;
@@ -13,7 +14,7 @@ const RoleDetail = () => {
             <div className="flex flex-col w-full gap-10">
                 <div className="flex w-full justify-between items-center">
                     <h1 className="text-3xl font-bold text-[#111827]">{role.name}</h1>
-                    <button className="h-[50px] w-[114px] border border-[#D1D5DB] hover:bg-[#D1D5DB]/20 text-[#374151] font-medium rounded-[6px] flex justify-center items-center gap-3">Edit <MdModeEditOutline className="text-[#6B7280] text-xl" /> </button>
+                    <button className="h-[50px] w-[114px] border border-[#D1D5DB] hover:bg-[#D1D5DB]/20 text-[#374151] font-medium rounded-[6px] flex justify-center items-center gap-3" onClick={() => navigate(`/roles/edit/${id}`, { replace: true })}>Edit <MdModeEditOutline className="text-[#6B7280] text-xl" /> </button>
                 </div>
                 <div className="flex flex-col w-full text-[#374151]">
                     <div className="p-6 flex border-b-2 border-[#E5E7EB]">
