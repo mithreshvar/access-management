187817845d1d38b38a5fa17257d6be221a894183
mithreshvar <mithreshvar@gmail.com>
2024-07-01 13:46:19 +0530

    update

--- src/components/User/Users.jsx
+++ src/components/User/Users.jsx
@@ -1,20 +1,32 @@
 // import React from 'react'
 
-import { Outlet } from "react-router-dom";
-import UserList from "./UserList";
+import { Outlet, useLoaderData } from "react-router-dom";
+import UserListCell from "./UserListCell";
 
 const Users = () => {
-  return (
-    <div className='flex'>
-        <div className="w-1/2">
-            <p>Users</p>
-            <UserList />
+
+    const userList = useLoaderData();
+
+    return (
+        <div className='flex'>
+            <div className="w-1/2 text-lg flex flex-col gap-y-[58px]">
+                <div className="flex justify-between items-center py-[27px] px-[24px] gap-x-[16px] bg-[#D5E6FB2B]">
+                    <p className=" font-medium  ">All Users</p>
+                    <p >Filter </p>
+                </div>
+                <div className=' overflow-y-auto'>
+                    {
+                        userList.map((user, index) => (
+                            <UserListCell key={index} name={user.name} role={user.role} id={user.id} />
+                        ))
+                    }
+                </div>
+            </div>
+            <div className="w-1/2">
+                <Outlet />
+            </div>
         </div>
-        <div className="w-1/2">
-            <Outlet />
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Users
