ed90fbe1c8986e3eb671cdfc1bcf5700727c183c
mithreshvar <mithreshvar@gmail.com>
2024-07-01 17:49:47 +0530

    edit functionalities added

--- src/components/Role/NewRoleOrEdit.jsx
+++ src/components/Role/NewRoleOrEdit.jsx
@@ -1,56 +1,72 @@
 // import React from 'react'
 
-import { useLoaderData, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
 
-const NewRoleOrEdit = ({ route = 'roles' }) => {
-    const { id } = useParams();
-    if (route === "new") {
-        return (
-            <div className="w-full h-full flex items-center justify-center ">
-                <div className=" w-full flex flex-col gap-y-[26px]">
-                    <div className="flex flex-col gap-y-[px]">
-                        <h1 className="text-[24px] font-medium ">Add New Role</h1>
-                        <p className="text-[18px] text-[#6B7280]">This information will be help you give access to other people in your team</p>
-                    </div>
-                    <div className="w-full bg-[#E5E7EB] h-[1px]"/>
+const NewRoleOrEdit = ({ route = 'edit' }) => {
 
-                    <div className="flex justify-between items-center">
-                        <h4 className="w-[40%] text-[18px] font-medium">Role Name</h4>
-                        <input type="text" className="w-[60%] h-[45px] border border-[#D1D5DB] rounded-[5px] px-[10px]"/>
-                    </div>
-                    <div className="w-full bg-[#E5E7EB] h-[1px]"/>
+    const [roleData, setRoleData] = useState(
+        route === "edit" ? 
+        useLoaderData() :
+        {
+            name: '',
+            description: '',
+            accesses: []
+        }
+    )
 
-                    <div className="flex justify-between items-center">
-                        <h4 className="w-[40%] text-[18px] font-medium">Role Name</h4>
-                        <textarea className="w-[60%] h-[100px] border border-[#D1D5DB] rounded-[5px] px-[10px] py-[7px]"/>
-                    </div>
-                    <div className="w-full bg-[#E5E7EB] h-[1px]"/>
+    useEffect(() => {
+        if (route === 'new') {
+            setRoleData({
+                name: '',
+                description: '',
+                accesses: []
+            })
+        }
+    }, [route]) 
 
-                    <div className="flex justify-between items-center">
-                        <h2 className="w-[40%] text-[24px] font-medium">Add Permissions</h2>
-                        <button className="w-fit h-[50px] px-[25px] py-[13px] bg-[#4F46E5] text-white rounded-[25px]">Add</button>
-                    </div>
-                    <div className="w-full bg-[#E5E7EB] h-[1px]"/>
+    function handleChange(e) {
+        const { id, value } = e.target;
+        setRoleData(prevState => ({
+            ...prevState,
+            [id]: value
+        }))
+    }
 
-                    <div className="text-[18px] flex justify-end items-center gap-x-[16px] ">
-                        <button className="w-fit h-[50px] px-[23px] border-2 border-[#D1D5DB] text-[#374151] rounded-[8px]">Cancel</button>
-                        <button className="w-fit h-[50px] px-[23px] bg-[#4F46E5] text-white rounded-[8px]">Save</button>
-                    </div>
+    return (
+        <div className="w-full h-full flex items-center justify-center ">
+            <div className=" w-full flex flex-col gap-y-[26px]">
+                <div className="flex flex-col gap-y-[px]">
+                    <h1 className="text-[24px] font-medium ">Add New Role</h1>
+                    <p className="text-[18px] text-[#6B7280]">This information will be help you give access to other people in your team</p>
+                </div>
+                <div className="w-full bg-[#E5E7EB] h-[1px]"/>
+
+                <div className="flex justify-between items-center">
+                    <h4 className="w-[40%] text-[18px] font-medium">Role Name</h4>
+                    <input id={'name'} type="text" value={roleData.name} onChange={handleChange} className="w-[60%] h-[45px] border border-[#D1D5DB] rounded-[5px] px-[10px]"/>
+                </div>
+                <div className="w-full bg-[#E5E7EB] h-[1px]"/>
+
+                <div className="flex justify-between items-center">
+                    <h4 className="w-[40%] text-[18px] font-medium">Role Name</h4>
+                    <textarea id={'description'} value={roleData.description} onChange={handleChange} className="w-[60%] h-[100px] border border-[#D1D5DB] rounded-[5px] px-[10px] py-[7px]"/>
+                </div>
+                <div className="w-full bg-[#E5E7EB] h-[1px]"/>
+
+                <div className="flex justify-between items-center">
+                    <h2 className="w-[40%] text-[24px] font-medium">Add Permissions</h2>
+                    <button className="w-fit h-[50px] px-[25px] py-[13px] bg-[#4F46E5] text-white rounded-[25px]">Add</button>
+                </div>
+                <div className="w-full bg-[#E5E7EB] h-[1px]"/>
+
+                <div className="text-[18px] flex justify-end items-center gap-x-[16px] ">
+                    <button className="w-fit h-[50px] px-[23px] border-2 border-[#D1D5DB] text-[#374151] rounded-[8px]">Cancel</button>
+                    <button className="w-fit h-[50px] px-[23px] bg-[#4F46E5] text-white rounded-[8px]">Save</button>
                 </div>
             </div>
-        );
-    } else {
-
-        if (!user) {
-            return (
-                <div>Role not found {route}</div>
-            );
-        } else {
-            return (
-                <div>Edit {id}</div>
-            )
-        }
-    }
+        </div>
+    );
 }
 
 export default NewRoleOrEdit
