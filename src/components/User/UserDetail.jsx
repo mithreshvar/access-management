6250b4ea1a83e9823156ef068aeee5704b6e6a0c
superb-shan <shankar.k2021csec@sece.ac.in>
2024-07-01 13:04:13 +0530

    detail structure ready

--- src/components/User/UserDetail.jsx
+++ src/components/User/UserDetail.jsx
@@ -6,7 +6,32 @@ const UserDetail = () => {
     //get the id from the URL
     const { id } = useParams();
   return (
-    <div>UserDetail of {id}</div>
+    <div className="flex justify-center items-center">
+        <div className="flex flex-col w-full py-3 px-6 gap-10">
+            <div className="flex w-full justify-between">
+                <h1 className="text-2xl font-bold">User Detail</h1>
+                <button>edit</button>
+            </div>
+            <div className="flex flex-col w-full">
+                <div className="p-6 flex border-b-2 ">
+                    <p className="w-[40%] font-semibold">Email ID</p>
+                    <p>email</p>
+                </div>
+                <div className="p-6 flex border-b-2 ">
+                    <p className="w-[40%] font-semibold">Phone Number</p>
+                    <p>email</p>
+                </div>
+                <div className="p-6 flex border-b-2 ">
+                    <p className="w-[40%] font-semibold">Assigned Role</p>
+                    <p>email</p>
+                </div>
+                <div className="p-6 flex border-b-2 ">
+                    <p className="w-[40%] font-semibold">Module Access & Permits</p>
+                    <p>email</p>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
